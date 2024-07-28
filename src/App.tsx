6dbb6df94ff42d3df59a7d171399821e04c1b5cf
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2024-07-28 22:41:05 +0900

    chore: readme update

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='vendor'>
+      <div className='vendor-layout'>
+        <div className='vendor-displays'>
+          <div className='vendor-displays-can'>
+
+          </div>
+          <div className='vendor-displays-price'>
+
+          </div>
+        </div>
+       
+      </div>
+    </div>
+    <div className='input'>
+
+    </div>
+    <div className="output">
+
+    </div>
+    </>
+  )
+}
+
+export default App
